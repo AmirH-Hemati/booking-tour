fd88694ff1009cbcd4fd46205fd2ee9c85228942
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-08-29 10:47:14 +0330

    creating a schema and routes  for the tour

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const path = require("path");
+const toursRouter = require("./routes/tourRoutes");
 const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/v1/tours", toursRouter);
 module.exports = app;
