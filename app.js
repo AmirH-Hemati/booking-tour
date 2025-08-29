986cbeff6d7d55bef89311c9b356e36bc669102e
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-08-29 11:48:19 +0330

    create middlware for not found routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const toursRouter = require("./routes/tourRoutes");
 const express = require("express");
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
 const app = express();
 if (process.env.NODE_DEV === "development") {
   app.use(morgan("tiny"));
@@ -10,4 +11,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/tours", toursRouter);
+
+app.use("/api/v1/users", usersRouter);
+
+app.use((req, res, next) => {
+  next(new AppError(`cant' find ${req.originalUrl} on this server!`, 400));
+});
+
 module.exports = app;
