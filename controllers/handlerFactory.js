365d0ddd6281d21dc59d24c4b1b32da8d13d48cb
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-08-29 11:14:49 +0330

    create handlerFactory for crud operation such as create , delete , getAll , getOne and update .

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,64 @@
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+exports.createOne = (Model) => {
+  return catchAsync(async (req, rex, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        data: doc,
+      },
+    });
+  });
+};
+
+exports.getAll = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const docs = await Model.find();
+
+    res.status(200).json({
+      status: "success",
+      data: { data: docs },
+    });
+  });
+};
+
+exports.getOne = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const doc = await Model.findById(req.params.id);
+    if (!doc) {
+      return new AppError("No document found  with that ID! ", 404);
+    }
+  });
+};
+
+exports.updateOne = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!doc) {
+      return new AppError("No document found  with that ID! ", 404);
+    }
+    res.status(200).json({
+      status: "success",
+      data: { data: doc },
+    });
+  });
+};
+
+exports.deleteOne = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+    if (!doc) {
+      return new AppError("No document found  with that ID! ", 404);
+    }
+    res.status(204).json({
+      status: "success",
+      data: { data: null },
+    });
+  });
+};
