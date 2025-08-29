3e271ec436c8a735a170910cf5c93b333a48a1a9
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-08-29 11:26:20 +0330

    add morgan for details each endPoint

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const path = require("path");
 const toursRouter = require("./routes/tourRoutes");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+if (process.env.NODE_DEV === "development") {
+  app.use(morgan("tiny"));
+}
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
