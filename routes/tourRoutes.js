fa3be4702d5aafa3d59ec5cabf5918d2b3dbd4bb
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-08-29 16:25:12 +0330

    connected to mongodb and to fix bug in tourRoutes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,12 +4,12 @@ const {
   createTour,
   updateTour,
   deleteTour,
+  getTour,
 } = require("../controllers/tourControllers");
-const { getOne } = require("../controllers/handlerFactory");
 
 const router = express.Router();
 
 router.route("/").get(getAllTours).post(createTour);
-router.route("/:id").get(getOne).patch(updateTour).delete(deleteTour);
+router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const app = require("./app");
 const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("successfully connected to mongDB"));
+
 app.listen(port, () => {
   console.log(`listen on port http://localhost:${port}`);
 });
